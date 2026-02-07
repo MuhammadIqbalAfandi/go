15404bda0af8c8762ea28c75cfb28a5db94763f3
Muhammad Iqbal Afandi <muhammadiqbalafandi30@gmail.com>
2026-02-07 08:43:55 +0700

    go-basic: add numbers

--- go-basic/number.go
+++ go-basic/number.go
@@ -5,7 +5,31 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("int = ", 1)
-	fmt.Println("int = ", 2)
-	fmt.Println("float32 = ", 3.5)
+	var i8 int8 = -8
+	var i int = -100
+
+	fmt.Println("==signed integer==")
+	fmt.Println("i8 = ", i8)
+	fmt.Println("i = ", i)
+
+	var u64 uint64 = 64
+	var u uint = 100
+
+	fmt.Println("==unsigned integer==")
+	fmt.Println("u64 = ", u64)
+	fmt.Println("u = ", u)
+
+	var b byte = 255
+	var rune = 'A'
+
+	fmt.Println("==byte and rune==")
+	fmt.Println("b = ", b)
+	fmt.Println("rune = ", rune)
+
+	var f32 float32 = 3.14
+	var f64 float64 = 3.14159265359
+
+	fmt.Println("==floating point==")
+	fmt.Println("f32 = ", f32)
+	fmt.Println("f64 = ", f64)
 }
